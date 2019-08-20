f9013a1893794e1075872d401bc90fad8171e887
veyselindi <veyselindi@gmail.com>
2019-08-20 12:40:22 +0200

    even

--- Week1/homework/app.js
+++ Week1/homework/app.js
@@ -4,8 +4,27 @@
   const bookTitles = [
     // Replace with your own book titles
     'harry_potter_chamber_secrets',
+    'lord_of_the_rings',
+    'life_is_good',
+    'ww_1',
+    'ww_2',
+    'econ_101',
+    'international_relations',
+    'toyota_mechanics',
+    'popular_mechanics',
+    'arab_spring',
   ];
-
-  // Replace with your own code
   console.log(bookTitles);
+
+  const newUl = document.createElement('ul');
+  bookTitles.forEach(book => {
+    const listItem = document.createElement('li');
+    const itemText = document.createTextNode(book);
+    listItem.appendChild(itemText);
+    newUl.appendChild(listItem);
+  });
+
+  console.log(newUl);
+
+  bookTitles.title;
 }
